c1ceb962c848b72e42de5374538e7b04de474616
eduardaraujo <mpmaeduarda2002@gmail.com>
2021-12-08 01:40:18 +0000

    nossa senhora mãe amada

---
+++ aed2122_p01/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/LENOVO/OneDrive/Ambiente de Trabalho/FEUP/AED/Práticas/P1/aed2122_p01/lib/googletest-master
+# Build directory: C:/Users/LENOVO/OneDrive/Ambiente de Trabalho/FEUP/AED/Práticas/P1/aed2122_p01/cmake-build-debug/lib/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
