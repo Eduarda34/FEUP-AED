a19de49135580c312599110888dc2530d50c5a00
eduardaraujo <mpmaeduarda2002@gmail.com>
2021-12-09 00:46:00 +0000

    Pratica 3

--- aed2122_p03/Tests/funSearchProblem.cpp
+++ aed2122_p03/Tests/funSearchProblem.cpp
@@ -4,23 +4,72 @@ FunSearchProblem::FunSearchProblem() {}
 
 //-----------------------------------------------------------------
 
-// TODO
+//1. (a)
 int FunSearchProblem::facingSun(const vector<int> & values) {
-    return 0;
+    int edif = 1; //Assumir que existe pelo menos um edifício
+
+    if(values.size() == 0) //Não existência de edifícios (vetor nulo)
+        edif = 0;
+
+    else{
+        for(int i=1; i < values.size(); i++){
+            if(values[i-1] < values[i]) //Compara o edifício com o seguinte
+                edif++;
+        }
+    }
+    return edif;
 }
 
-// TODO
+//1. (b)
 int FunSearchProblem::squareR(int num) {
-    return 0;
+    int inicial = 0;
+    int final = num;
+    int centro;
+
+    while (inicial != final){
+        centro = (inicial + final) / 2;
+        if(centro * centro < num){
+            if(inicial == centro)
+                return centro;
+            inicial = centro;
+        }
+
+        if(centro * centro > num)
+            final = centro;
+
+        if(centro * centro == num){
+            inicial = final = centro;
+        }
+    }
+    return centro;
 }
 
-// TODO
+//1. (c)
 int FunSearchProblem::smallestMissingValue(const vector<int> & values) {
     return 0;
 }
 
-// TODO
+//1. (d) *exercício extra*
 int FunSearchProblem::minPages(const vector<int> & values, int numSt) {
-    return 0;
+    int sum = 0;
+    int const refValue = sum / numSt;
+    int pagAtual = 0, numLivros = 0;
+
+    for(auto const pages : values)
+        sum += pages;
+
+    for(int i=0; i < values.size()-1; i++){
+        pagAtual += values[i];
+
+        if(pagAtual <= refValue || numLivros == 0)
+            numLivros++;
+
+        else{
+            cout << "The student " << numSt << " has " << numLivros << " books with " << pagAtual << " pages\n";
+            numSt--;
+            pagAtual = numLivros = 0;
+        }
+    }
+    return -1;
 }
 
